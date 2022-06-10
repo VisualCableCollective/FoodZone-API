2c09a5138d44d4c864288e20b0b9993a3b9c3305
Joschua H <joschua.hass.sh@gmail.com>
2022-06-10 10:18:44 +0200

    fix: php doc

--- app/Models/Seller.php
+++ app/Models/Seller.php
@@ -3,39 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Carbon;
 
 /**
  * A seller account offers various products on the plattform and represents one company (with multiple locations).
  * It can be managed by multiple persons
  *
- * @property string id as uuid
- * @property string name
- * @property Carbon created_at
- * @property Carbon updated_at
+ * @property string $id as uuid
+ * @property string $name
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
  */
-class Seller extends Model
+class Seller extends BaseUuidModel
 {
     use HasFactory;
-
-    /**
-     * Get the value indicating whether the IDs are incrementing.
-     *
-     * @return bool
-     */
-    public function getIncrementing()
-    {
-        return false;
-    }
-
-    /**
-     * Get the auto-incrementing key type.
-     *
-     * @return string
-     */
-    public function getKeyType()
-    {
-        return 'string';
-    }
 }
