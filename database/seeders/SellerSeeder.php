732b72a524d1c7bea71c55e3c7bcee4e39f7aa2e
Joschua H <joschua.hass.sh@gmail.com>
2022-06-21 10:13:39 +0200

    fix(SellerSeeder): invalid Sequence param

--- database/seeders/SellerSeeder.php
+++ database/seeders/SellerSeeder.php
@@ -20,14 +20,14 @@ class SellerSeeder extends Seeder
     {
         $convCatNames = [];
         foreach (ProductCategory::factory()->getCategoryNames() as $catName) {
-            $convCatNames[] = ["name" => $catName];
+            $convCatNames[] = $catName;
         }
 
         foreach (Seller::factory()->getSellerNames() as $companyName) {
             Seller::factory(1)
                 ->has(Location::factory()->count(rand(1, 4)))
                 ->has(ProductCategory::factory()->count(rand(3, count(ProductCategory::factory()->getCategoryNames())))
-                    ->state(new Sequence($convCatNames))
+                    ->state(new Sequence(fn ($sequence) => ['name' => next($convCatNames)],))
                 )
                 ->create([
                 "name" => $companyName
